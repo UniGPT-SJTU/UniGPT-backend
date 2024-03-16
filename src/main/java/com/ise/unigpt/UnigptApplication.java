ef71c3c5d0a99b99dc8506dd0d570ac2079f981a
creeper <creeperhjt@sjtu.edu.cn>
2024-03-16 15:38:06 +0800

    Initial Commit

---
+++ src/main/java/com/ise/unigpt/UnigptApplication.java
@@ -0,0 +1,13 @@
+package com.ise.unigpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnigptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnigptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ise/unigpt/UnigptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ise.unigpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnigptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
