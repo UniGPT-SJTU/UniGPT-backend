ec3e00783c4c70595dc3a57c8c365dec23f86dea
kiwi142857 <779490624@qq.com>
2024-05-06 22:33:16 +0800

    可以通过ws访问，传回信息目前写死

--- src/main/java/com/ise/unigpt/config/WebSocketConfig.java
+++ src/main/java/com/ise/unigpt/config/WebSocketConfig.java
@@ -1,10 +1,13 @@
 package com.ise.unigpt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.ise.unigpt.service.LLMService;
+import com.ise.unigpt.serviceimpl.OpenAIService;
 import com.ise.unigpt.websocket.ChatWebSocketHandler;
 
 import org.springframework.lang.NonNull;
@@ -12,6 +15,7 @@ import org.springframework.lang.NonNull;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
     @Override
     public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
         registry.addHandler(new ChatWebSocketHandler(), "/chat");
--- src/main/java/com/ise/unigpt/websocket/ChatWebSocketHandler.java
+++ src/main/java/com/ise/unigpt/websocket/ChatWebSocketHandler.java
@@ -8,10 +8,28 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import io.micrometer.common.lang.NonNull;
 import jakarta.servlet.http.Cookie;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.ise.unigpt.model.Chat;
+import com.ise.unigpt.model.ChatType;
+import com.ise.unigpt.model.PromptChat;
+import com.ise.unigpt.model.PromptChatType;
+import com.ise.unigpt.service.LLMService;
+import com.ise.unigpt.serviceimpl.OpenAIService;
+
 public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private final LLMService llmService;
+
+    public ChatWebSocketHandler() {
+        this.llmService = new OpenAIService();
+    }
+
     /*
      * @Override
      * public void afterConnectionEstablished(WebSocketSession session) throws
@@ -86,5 +104,41 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 
         // 现在你可以根据需要处理payload
         System.out.println("Received message: " + payload);
+        // TODO: 将payload转化成json对应的DTO对象
+        // 目前先用testChat()方法的返回值作为回复
+        String response = testChat();
+        System.out.println("Sending response: " + response);
+        try {
+            // 发送回复消息
+            session.sendMessage(new TextMessage(response));
+            session.close(CloseStatus.NORMAL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    String testChat() {
+        List<PromptChat> promptChatList = new ArrayList<>();
+        // 机器人创建时的模板对话
+        promptChatList.add(new PromptChat(PromptChatType.SYSTEM, "你是一个++{lang}大师，精通++{lang}的所有语法和库。"));
+        promptChatList.add(new PromptChat(PromptChatType.USER, "如何使用++{lang}++{question}？"));
+
+        // 历史在创建时，用户填入的提示词键值对
+        Map<String, String> promptList = Map.of(
+                "lang", "java",
+                "question", "计算1-100的和");
+
+        // 用户创建历史之后，和机器人的对话
+        List<Chat> chatList = new ArrayList<>();
+        // 机器人回答
+        chatList.add(new Chat(ChatType.BOT,
+                "你可以使用一个简单的for循环来计算1到100这一范围的所有数字的和。以下是一个示例代码： ```java public class CalculateSum { public static void main(String[] args) { int sum = 0; for (int i = 1; i <= 100; i++) { sum += i; } System.out.println(\"1到100的和为：\" + sum); } } ``` 在这个示例中，我们初始化一个变量`sum`为0，然后使用for循环从1累加到100，最后打印出1到100的和。你可以运行这段代码来得到结果。"));
+        // 用户追问
+        chatList.add(new Chat(ChatType.USER, "有没有更加简洁的求和方式？"));
+        try {
+            return llmService.generateResponse(promptChatList, promptList, chatList);
+        } catch (Exception e) {
+            return "error met";
+        }
     }
 }
