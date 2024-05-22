1619bd700d5101aa4b474c1b5f05d2e6f4959ef5
BLeaves <chlorophyll2020@sjtu.edu.cn>
2024-05-22 18:19:29 +0800

    GET /api/histories/{historyid}/chats 时增加 type 属性供前端判断使用

--- src/main/java/com/ise/unigpt/dto/ChatDTO.java
+++ src/main/java/com/ise/unigpt/dto/ChatDTO.java
@@ -14,11 +14,13 @@ public class ChatDTO {
     private Date time;
     private String avatar;
     private String name;
+    private ChatType type;
 
     public ChatDTO(Chat chat) {
         this.id = chat.getId();
         this.content = chat.getContent();
         this.time = chat.getTime();
+        this.type = chat.getType();
 
         History history = chat.getHistory();
         this.avatar = chat.getType() == ChatType.BOT ?
