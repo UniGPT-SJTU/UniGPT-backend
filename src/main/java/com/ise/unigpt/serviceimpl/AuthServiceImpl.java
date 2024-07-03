8eda7fa9de5bff69a815ed759df45d08b341a590
kiwi142857 <779490624@qq.com>
2024-07-03 11:21:00 +0800

    恢复jaccount login

--- src/main/java/com/ise/unigpt/serviceimpl/AuthServiceImpl.java
+++ src/main/java/com/ise/unigpt/serviceimpl/AuthServiceImpl.java
@@ -1,18 +1,28 @@
 package com.ise.unigpt.serviceimpl;
 
+import java.net.HttpURLConnection;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import javax.naming.AuthenticationException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ise.unigpt.dto.JaccountResponseDTO;
+import com.ise.unigpt.dto.LoginRequestDTO;
+import com.ise.unigpt.dto.RegisterRequestDTO;
+import com.ise.unigpt.exception.UserDisabledException;
 import com.ise.unigpt.model.Auth;
 import com.ise.unigpt.model.User;
 import com.ise.unigpt.repository.AuthRepository;
 import com.ise.unigpt.repository.UserRepository;
 import com.ise.unigpt.service.AuthService;
-import com.ise.unigpt.exception.UserDisabledException;
-
-
-import org.springframework.stereotype.Service;
-
-import javax.naming.AuthenticationException;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -24,7 +34,25 @@ public class AuthServiceImpl implements AuthService {
         this.userRepository = userRepository;
     }
 
-    public String generateAuthToken(User user) {
+    public String login(LoginRequestDTO dto) throws AuthenticationException {
+        Optional<User> optionalUser = userRepository.findByName(dto.getUsername());
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getPassword().equals(dto.getPassword())) {
+                return generateAuthToken(user);
+            }
+        }
+        throw new AuthenticationException("Invalid username or password");
+    }
+
+    public void register(RegisterRequestDTO dto) {
+        if (userRepository.findByName(dto.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+        userRepository.save(new User(dto));
+    }
+
+    private String generateAuthToken(User user) {
         Optional<Auth> optionalAuth = authRepository.findByUser(user);
         Auth auth;
         if (optionalAuth.isPresent()) {
@@ -45,20 +73,99 @@ public class AuthServiceImpl implements AuthService {
         Auth auth = authRepository.findByToken(token)
                 .orElseThrow(() -> new NoSuchElementException("Invalid token"));
         User user = auth.getUser();
-        if (user.getDisabled()) throw new UserDisabledException("User is disabled");
+        if (user.getDisabled()) {
+            throw new UserDisabledException("User is disabled");
+        }
         return user;
     }
 
+    public String requestAccessToken(String code) throws AuthenticationException {
+        String client_id = "ov3SLrO4HyZSELxcHiqS";
+        String client_secret = "B9919DDA3BD9FBF7ADB9F84F67920D8CB6528620B9586D1C";
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = null;
+        try {
+            response = Unirest.post("http://jaccount.sjtu.edu.cn/oauth2/token")
+                    .header("Authorization",
+                            "Basic b3YzU0xyTzRIeVpTRUx4Y0hpcVM6Qjk5MTlEREEzQkQ5RkJGN0FEQjlGODRGNjc5MjBEOENCNjUyODYyMEI5NTg2RDFD")
+                    .header("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("Accept", "*/* ")
+                    .header("Host", "jaccount.sjtu.edu.cn")
+                    .header("Connection", "keep-alive")
+                    .field("grant_type", "authorization_code")
+                    .field("code", code)
+                    .field("client_id", client_id)
+                    .field("client_secret", client_secret)
+                    .field("redirect_uri", "http://localhost:3000/login")
+                    .asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject responseBody = null;
+        try {
+            responseBody = new JSONObject(response.getBody());
+        } catch (JSONException e) {
+            throw new AuthenticationException("Request access token failed");
+        }
+        String accessToken;
+        try {
+            accessToken = responseBody.getString("access_token");
+        } catch (JSONException e) {
+            throw new AuthenticationException("Request access token failed");
+        }
+        return accessToken;
+    }
+
     public String jaccountLogin(String code) throws AuthenticationException {
-        User user = new User();
+        String accessToken;
+        try {
+            accessToken = requestAccessToken(code);
+        } catch (AuthenticationException e) {
+            throw new AuthenticationException("Jaccount login failed");
+        }
+
+        User user = sendGetRequest("https://api.sjtu.edu.cn/v1/me/profile?access_token=" + accessToken);
         // System.out.println("user" + user);
         // 查找是否已经注册
         Optional<User> optionalUser = userRepository.findByAccount(user.getAccount());
-        if (optionalUser.isPresent()) return generateAuthToken(optionalUser.get());
+        if (optionalUser.isPresent()) {
+            return generateAuthToken(optionalUser.get());
+        }
         userRepository.save(user);
         String token = generateAuthToken(user);
         System.out.println("token" + token);
         return token;
     }
 
+    private User sendGetRequest(String urlStr) throws AuthenticationException {
+        try {
+            // System.out.println("urlStr: " + urlStr);
+            Unirest.setTimeouts(300, 3000);
+            HttpResponse<String> response = Unirest.get(urlStr)
+                    .header("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+                    .header("Accept", "*/*")
+                    .header("Host", "api.sjtu.edu.cn")
+                    .header("Connection", "keep-alive")
+                    .asString();
+
+            // System.out.println("Response status: " + response.getStatus());
+            // System.out.println("Response body: " + response.getBody());
+
+            if (response.getStatus() == HttpURLConnection.HTTP_OK) {
+                ObjectMapper mapper = new ObjectMapper();
+                JaccountResponseDTO jaccountResponse = mapper.readValue(response.getBody(), JaccountResponseDTO.class);
+
+                return new User(jaccountResponse.getUsers().get(0));
+            } else {
+                // System.out.println("GET request not worked");
+                throw new AuthenticationException("GET request not worked");
+            }
+
+        } catch (Exception e) {
+            // System.out.println("Exception: " + e.getMessage());
+            throw new AuthenticationException("Sending GET request failed");
+        }
+    }
 }
--- src/main/java/com/ise/unigpt/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/ise/unigpt/serviceimpl/UserServiceImpl.java
@@ -1,21 +1,25 @@
 package com.ise.unigpt.serviceimpl;
 
-import com.ise.unigpt.model.User;
-import com.ise.unigpt.dto.*;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.stereotype.Service;
+
+import com.ise.unigpt.dto.BotBriefInfoDTO;
+import com.ise.unigpt.dto.GetBotsOkResponseDTO;
+import com.ise.unigpt.dto.GetUsersOkResponseDTO;
+import com.ise.unigpt.dto.UpdateUserInfoRequestDTO;
+import com.ise.unigpt.dto.UserBriefInfoDTO;
 import com.ise.unigpt.model.Bot;
+import com.ise.unigpt.model.User;
 import com.ise.unigpt.repository.UserRepository;
 import com.ise.unigpt.service.AuthService;
 import com.ise.unigpt.service.UserService;
 import com.ise.unigpt.utils.PaginationUtils;
-
-import org.springframework.stereotype.Service;
-
-import javax.security.sasl.AuthenticationException;
-import java.util.Collections;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -36,12 +40,16 @@ public class UserServiceImpl implements UserService {
         return optionalUser.get();
     }
 
-    public boolean updateUserInfo(
+    public void updateUserInfo(
             Integer id,
             UpdateUserInfoRequestDTO updateUserInfoRequestDTO,
             String token) throws AuthenticationException {
+        Optional<User> optionalUser = repository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new NoSuchElementException("User not found for ID: " + id);
+        }
 
-        User targetUser = findUserById(id);
+        User targetUser = optionalUser.get();
         User requestUser = authService.getUserByToken(token);
         if (!targetUser.equals(requestUser)) {
             throw new AuthenticationException("Unauthorized to update user info");
@@ -52,39 +60,47 @@ public class UserServiceImpl implements UserService {
         targetUser.setCanvasUrl(updateUserInfoRequestDTO.getCanvasUrl());
 
         repository.save(targetUser);
-        return true;
     }
 
     // TODO: 修改BotBriefInfoDTO.asCreator
     public GetBotsOkResponseDTO getUsedBots(Integer userid, String token, Integer page, Integer pageSize)
             throws AuthenticationException {
-        User user = findUserById(userid);
+        Optional<User> optionalUser = repository.findById(userid);
+        if (optionalUser.isEmpty()) {
+            throw new NoSuchElementException("User with id " + userid + " not found");
+        }
 
-        if (!authService.getUserByToken(token).equals(user)) {
+        if (!authService.getUserByToken(token).equals(optionalUser.get())) {
             throw new AuthenticationException("Unauthorized to get used bots");
         }
 
-        List<Bot> usedBots = user.getUsedBots();
-        Collections.reverse(usedBots);
+        List<Bot> usedBots = optionalUser.get().getUsedBots();
 
         List<BotBriefInfoDTO> bots = usedBots.stream()
                 .map(bot -> new BotBriefInfoDTO(bot.getId(), bot.getName(), bot.getDescription(), bot.getAvatar(),
-                        bot.getCreator().equals(user), user.getAsAdmin()))
+                        bot.getCreator().equals(optionalUser.get()), optionalUser.get().getAsAdmin()))
                 .collect(Collectors.toList());
 
         return new GetBotsOkResponseDTO(bots.size(), PaginationUtils.paginate(bots, page, pageSize));
     }
 
     // TODO: 修改BotBriefInfoDTO.asCreator
-    public GetBotsOkResponseDTO getStarredBots(Integer userid, String token, Integer page, Integer pageSize) {
-        User user = findUserById(userid);
+    public GetBotsOkResponseDTO getStarredBots(Integer userid, String token, Integer page, Integer pageSize)
+            throws AuthenticationException {
+        Optional<User> optionalUser = repository.findById(userid);
+        if (optionalUser.isEmpty()) {
+            throw new NoSuchElementException("User with id " + userid + " not found");
+        }
 
-        List<Bot> starredBots = user.getStarBots();
-        Collections.reverse(starredBots);
+        if (!authService.getUserByToken(token).equals(optionalUser.get())) {
+            throw new AuthenticationException("Unauthorized to get used bots");
+        }
+
+        List<Bot> starredBots = optionalUser.get().getStarBots();
 
         List<BotBriefInfoDTO> bots = starredBots.stream()
                 .map(bot -> new BotBriefInfoDTO(bot.getId(), bot.getName(), bot.getDescription(), bot.getAvatar(),
-                        bot.getCreator().equals(user), user.getAsAdmin()))
+                        bot.getCreator().equals(optionalUser.get()), optionalUser.get().getAsAdmin()))
                 .collect(Collectors.toList());
 
         return new GetBotsOkResponseDTO(bots.size(), PaginationUtils.paginate(bots, page, pageSize));
@@ -92,9 +108,12 @@ public class UserServiceImpl implements UserService {
 
     // TODO: 修改BotBriefInfoDTO.asCreator
     public GetBotsOkResponseDTO getCreatedBots(Integer userid, String token, Integer page, Integer pageSize) {
-        User user = findUserById(userid);
-        List<Bot> createdBots = user.getCreateBots();
-        Collections.reverse(createdBots);
+        Optional<User> optionalUser = repository.findById(userid);
+        if (optionalUser.isEmpty()) {
+            throw new NoSuchElementException("User with id " + userid + " not found");
+        }
+        User user = optionalUser.get();
+        List<Bot> createdBots = optionalUser.get().getCreateBots();
 
         List<BotBriefInfoDTO> bots = createdBots.stream()
                 .map(bot -> new BotBriefInfoDTO(bot.getId(), bot.getName(), bot.getDescription(), bot.getAvatar(),
@@ -149,7 +168,7 @@ public class UserServiceImpl implements UserService {
                 PaginationUtils.paginate(userBriefInfoDTOs, page, pagesize));
     }
 
-    public boolean setBanUser(Integer id, String token, Boolean state) throws AuthenticationException {
+    public void setBanUser(Integer id, String token, Boolean state) throws AuthenticationException {
         User requestUser;
         try {
             requestUser = authService.getUserByToken(token);
@@ -160,11 +179,13 @@ public class UserServiceImpl implements UserService {
         if (!requestUser.getAsAdmin()) {
             throw new AuthenticationException("Unauthorized to ban user");
         }
-
-        User targetUser = findUserById(id);
+        Optional<User> optionalUser = repository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new NoSuchElementException("User with id " + id + " not found");
+        }
+        User targetUser = optionalUser.get();
         targetUser.setDisabled(state);
         repository.save(targetUser);
-        return true;
     }
 
     public Boolean getBanState(Integer id, String token) throws AuthenticationException {
@@ -177,7 +198,11 @@ public class UserServiceImpl implements UserService {
         if (!requestUser.getAsAdmin()) {
             throw new AuthenticationException("Unauthorized to get ban state");
         }
-        User user = findUserById(id);
-        return user.getDisabled();
+
+        Optional<User> optionalUser = repository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new NoSuchElementException("User with id " + id + " not found");
+        }
+        return optionalUser.get().getDisabled();
     }
 }
