8b7be52ccc823c6f5cbb5456a291ff0bc2ead639
BLeaves <chlorophyll2020@sjtu.edu.cn>
2024-07-05 16:01:41 +0800

    Complete file store

--- src/main/java/com/ise/unigpt/serviceimpl/KnowledgeServiceImpl.java
+++ src/main/java/com/ise/unigpt/serviceimpl/KnowledgeServiceImpl.java
@@ -10,28 +10,36 @@ import com.ise.unigpt.repository.UserRepository;
 import com.ise.unigpt.service.AuthService;
 import com.ise.unigpt.service.ChatHistoryService;
 import com.ise.unigpt.service.KnowledgeService;
-//import dev.langchain4j.data.document.Document;
-//import dev.langchain4j.data.document.DocumentSplitter;
-//import dev.langchain4j.data.document.splitter.DocumentSplitters;
-//import dev.langchain4j.data.segment.TextSegment;
-//import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-//import dev.langchain4j.store.embedding.EmbeddingStore;
-//import dev.langchain4j.store.embedding.pgvector.PgVectorEmbeddingStore;
+
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.naming.AuthenticationException;
-//import java.util.List;
-//import java.util.NoSuchElementException;
-//
-//import org.apache.pdfbox.pdmodel.PDDocument;
-//import org.apache.pdfbox.text.PDFTextStripper;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
+import java.util.List;
+import java.util.NoSuchElementException;
 
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+// pgvector
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.pgvector.PgVectorEmbeddingStore;
 @Service
 public class KnowledgeServiceImpl implements KnowledgeService{
 
@@ -42,7 +50,6 @@ public class KnowledgeServiceImpl implements KnowledgeService{
     private final PromptChatRepository promptChatRepository;
     private final AuthService authService;
     private final ChatHistoryService chatHistoryService;
-    EmbeddingModel embeddingModel;
 
     public KnowledgeServiceImpl(BotRepository botRepository,
                           UserRepository userRepository,
@@ -56,79 +63,79 @@ public class KnowledgeServiceImpl implements KnowledgeService{
         this.authService = authService;
         this.historyRepository = historyRepository;
         this.chatHistoryService = chatHistoryService;
-        //this.embeddingModel = new AllMiniLmL6V2EmbeddingModel();
     }
-//
-//    public String extractText(MultipartFile file) throws IOException {
-//        String fileName = file.getOriginalFilename();
-//        if (fileName != null && fileName.toLowerCase().endsWith(".pdf")) {
-//            return extractTextFromPdf(file);
-//        } else if (fileName != null && fileName.toLowerCase().endsWith(".txt")) {
-//            return extractTextFromTxt(file);
-//        } else {
-//            throw new IllegalArgumentException("Unsupported file type: " + fileName);
-//        }
-//    }
-//
-//    private String extractTextFromPdf(MultipartFile file) throws IOException {
-//        PDDocument document = PDDocument.load(file.getInputStream());
-//        PDFTextStripper pdfStripper = new PDFTextStripper();
-//        String text = pdfStripper.getText(document);
-//        document.close();
-//        return text;
-//    }
-//
-//    private String extractTextFromTxt(MultipartFile file) throws IOException {
-//        StringBuilder textBuilder = new StringBuilder();
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                textBuilder.append(line).append(System.lineSeparator());
-//            }
-//        }
-//        return textBuilder.toString();
-//    }
+
+    public String extractText(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        if (fileName != null && fileName.toLowerCase().endsWith(".pdf")) {
+            return extractTextFromPdf(file);
+        } else if (fileName != null && fileName.toLowerCase().endsWith(".txt")) {
+            return extractTextFromTxt(file);
+        } else {
+            throw new IllegalArgumentException("Unsupported file type: " + fileName);
+        }
+    }
+
+    private String extractTextFromPdf(MultipartFile file) throws IOException {
+        try (PDDocument document = PDDocument.load(file.getInputStream())) {
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            return pdfStripper.getText(document);
+        }
+    }
+
+    private String extractTextFromTxt(MultipartFile file) throws IOException {
+        StringBuilder textBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                textBuilder.append(line).append(System.lineSeparator());
+            }
+        }
+        return textBuilder.toString();
+    }
 
     public ResponseDTO uploadFile(Integer id, String token, MultipartFile file) throws AuthenticationException {
-//        Bot bot = botRepository.findById(id)
-//                .orElseThrow(() -> new NoSuchElementException("Bot not found for ID: " + id));
-//
-//        User user;
-//        try {
-//            user = authService.getUserByToken(token);
-//        } catch (NoSuchElementException e) { throw new NoSuchElementException("User not found");}
-//
-//        if(!bot.getCreator().equals(user) && !user.getAsAdmin()){
-//            throw new AuthenticationException("Unauthorized to upload file.");
-//        }
-//
-//        try{
-//            Document document = new Document(extractText(file));
-//            DocumentSplitter splitter = DocumentSplitters.recursive(
-//                    maxSegmentSizeInChar,
-//                    0
-//            );
-//            List<TextSegment> textSegmentList = splitter.split(document);
-//
-//            EmbeddingStore<TextSegment> embeddingStore = PgVectorEmbeddingStore.builder()
-//                    .host("localhost")
-//                    .port(5432)
-//                    .database("rag")
-//                    .user("bleaves")
-//                    .password("bleaves")
-//                    .table("bot" + id)
-//                    .dimension(dimension)
-//                    .build();
-//
-//            for(TextSegment segment: textSegmentList)
-//                embeddingStore.add(
-//                        embeddingModel.embed(segment).content(),
-//                        segment
-//                );
-//        }
-//        catch (Exception e){
-//            throw new IllegalArgumentException(e.getMessage());
-//        }
+        Bot bot = botRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Bot not found for ID: " + id));
+
+        User user;
+        try {
+            user = authService.getUserByToken(token);
+        } catch (NoSuchElementException e) { throw new NoSuchElementException("User not found");}
+
+        if(!bot.getCreator().equals(user) && !user.getAsAdmin()){
+            throw new AuthenticationException("Unauthorized to upload file.");
+        }
+
+        try{
+            Document document = new Document(extractText(file));
+            DocumentSplitter splitter = DocumentSplitters.recursive(
+                    maxSegmentSizeInChar,
+                    0
+            );
+            List<TextSegment> textSegmentList = splitter.split(document);
+
+            EmbeddingStore<TextSegment> embeddingStore = PgVectorEmbeddingStore.builder()
+                    .host("localhost")
+                    .port(5432)
+                    .database("rag")
+                    .user("bleaves")
+                    .password("bleaves")
+                    .table("bot" + id)
+                    .dimension(dimension)
+                    .build();
+
+            EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+            for(TextSegment segment: textSegmentList)
+                embeddingStore.add(
+                        embeddingModel.embed(segment).content(),
+                        segment
+                );
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
 
         return new ResponseDTO(true, "Successfully upload " + file.getOriginalFilename());
     }
