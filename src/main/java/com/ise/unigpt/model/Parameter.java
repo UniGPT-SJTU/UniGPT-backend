372363d5514873d151a2c317a1ac0fac386c92a5
Kiwi <779490624@qq.com>
2024-07-08 14:26:42 +0800

    Add entity of plugin: tool & parameter

---
+++ src/main/java/com/ise/unigpt/model/Parameter.java
@@ -0,0 +1,34 @@
+package com.ise.unigpt.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "parameter")
+public class Parameter {
+    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 3000)
+    private String description;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @ManyToOne()
+    @JoinColumn(name = "tool_id", nullable = false)
+    private Tool tool;
+}
